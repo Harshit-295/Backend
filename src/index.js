43f3636a97015280b23b6b6465ba4bb0bbc476f6
Harshit-295 <bhardwajharsh1984@gmail.com>
2025-02-08 07:33:03 +0530

    add user and vedio models

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "./constants.js";
+
+import connectDB from "./db/index.js";
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`Server is runnig at port : ${process.env.PORT}`)
+    })
+})
+
+.catch((err)=>{
+    console.log("MOngo db connection failed!!!",err)
+})
+// import express from "express"
+// const app = express()
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",()=>{
+//             console.log("ERRR",error)
+//         })
+//         app.listen(process.env.PORT , ()=>{
+//             console.log(`App is listeing on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.log(error<"ERROR")
+//         throw err
+//     }
+// })()
---
+++ src/models/vedio.model.js
@@ -0,0 +1,43 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const vedioSchema = new Schema(
+    {
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User"
+        },
+        vedioFile:{
+            type:String,//cloudinary url
+            required:true
+        },
+        thumbnail:{
+            type:String,//cloundinary url
+            required:true
+        },
+        title:{
+            type:String,//cloundinary url
+            required:true
+        },
+        description:{
+            type:String,
+            required:true
+        },
+        views:{
+            type:Number,
+            default:0
+        },
+        duration:{
+            type:Number,
+            required:true
+        },
+        isPublished:{
+          type:Boolean,
+          default:true
+        }
+    },{timestamps:true}
+)
+vedioSchema.plugin(mongooseAggregatePaginate)
+
+export const Vedio = mongoose.model("Vedio",vedioSchema)
